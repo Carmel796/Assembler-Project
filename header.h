78ea95dbf1628c0b5c663b27066ec84ef18c1ca8
כרמל כהן <carmel@krmls-Mac-mini.local>
2024-07-30 15:35:14 +0300

    check point

--- header.h
+++ header.h
@@ -2,7 +2,7 @@
 #define HEADER_H
 
 FILE *fopen_with_ending(char *source_file_name, char *ending, char *mode);
-FILE *macro_search(FILE *as_file, FILE *am_file , hash_table macros);
+FILE *macro_search(FILE *as_file, FILE *am_file , hashtable macros);
 void handle_macro(char *line, int offset);
 void add_to_hash(char *key, char *value);
 
